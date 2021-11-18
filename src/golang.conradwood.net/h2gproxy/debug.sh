8ebd54e45dfc1bcae1d6532a14c372601fabc8f5
Conrad Wood <cnw@conradwood.net>
2021-11-18 10:19:42 +0000

    .
    Change-Id: I38f0fc877799f38209c548252430840d9523d5b2

--- src/golang.conradwood.net/h2gproxy/debug.sh
+++ src/golang.conradwood.net/h2gproxy/debug.sh
@@ -4,3 +4,4 @@
 module-stop localhost:4177 
 make server || exit 10
 sudo ${GOBIN}/h2gproxy-server -registry=localhost -token=${H2GPROXY_TOKEN} -config_file=${HOME}/devel/go/h2gproxy/configs/cnw.yaml -port=4177 -http_port=80 -https_port=443 -enable_histogram=true -enable_basic_auth=true -enable_raw_paths=true -log_each_request=false -registry_resolver=localhost -ge_dialer_sleep_time=1 -ge_deployment_descriptor=V1:h2gproxy/testing/h2gproxy/100000 -disable_tcp -debug -print_headers -log_each_request -debug_grpc_proxy -print_timing -activate_probe_backend
+
