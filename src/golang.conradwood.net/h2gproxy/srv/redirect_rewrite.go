d95f7101cf5526a498e5ebe0d51de468fec1ce6e
Conrad Wood <cnw@conradwood.net>
2023-09-15 14:14:09 +0100

    automatic commit
    Change-Id: I91583c3b8accb1dece9c6c27cdf17f393b325e98

---
+++ src/golang.conradwood.net/h2gproxy/srv/redirect_rewrite.go
@@ -0,0 +1,36 @@
+package srv
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (h *HTTPForwarder) IsRedirectMatcher() bool {
+	if len(h.def.RedirectRewrites) == 0 {
+		return false
+	}
+	return true
+}
+func RedirectRewrite(f *FProxy) {
+	u := f.FullURL()
+	fmt.Printf("inbound url: \"%s\"\n", u)
+	for _, rr := range f.hf.def.RedirectRewrites {
+		ou := u
+		u = strings.ReplaceAll(u, rr.MatchString, rr.ReplaceWith)
+		if u != ou && rr.SetHost != "" {
+			u = strings.TrimPrefix(u, "http://")
+			u = strings.TrimPrefix(u, "https://")
+			idx := strings.Index(u, "/")
+			u = u[idx:]
+			u = strings.TrimPrefix(u, "/")
+			fmt.Printf("u without host: \"%s\"\n", u)
+			u = "https://" + rr.SetHost + "/" + strings.TrimPrefix(u, "/")
+		}
+	}
+	q := f.req.URL.RequestURI() // everything after the host (inc path)
+	idx := strings.Index(q, "?")
+	q = q[idx:]
+	u = u + q
+	fmt.Printf("redirecting to: \"%s\"\n", u)
+	f.RedirectTo(u, false)
+}
