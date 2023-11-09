2138043fddf2e3303e53a846d93f7804503c353b
Conrad Wood <cnw@conradwood.net>
2023-11-09 09:20:22 +0000

    also ask session manager if the token is valid

--- src/golang.conradwood.net/h2gproxy/srv/cookie-to-auth.go
+++ src/golang.conradwood.net/h2gproxy/srv/cookie-to-auth.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	apb "golang.conradwood.net/apis/auth"
 	"golang.conradwood.net/go-easyops/common"
+	"golang.conradwood.net/go-easyops/utils"
+	"golang.yacloud.eu/apis/sessionmanager"
 	"net/http"
 	"sync"
 )
@@ -28,6 +30,10 @@ func GetUserFromCookie(c *http.Cookie) *apb.SignedUser {
 	return user
 }
 
+// if token is invalid - returns nil,nil
+// if something goes wrong resolving token, returns nil,error
+// if token resolves to a valid and authenticated user, returns user,nil
+
 func TokenToUser(token string) (*apb.SignedUser, error) {
 	uc := cache_GetUserByToken(token)
 	if uc != nil {
@@ -40,9 +46,28 @@ func TokenToUser(token string) (*apb.SignedUser, error) {
 	if uc != nil {
 		return uc.SignedAuthUser, uc.Error
 	}
+	var user_result *apb.SignedUser
+	var gdr *apb.SignedAuthResponse
+
+	// check the session manager
+	st := &sessionmanager.SessionToken{Token: token}
+	ctx := createBootstrapContext()
+	sv, err := sessionmanager.GetSessionManagerClient().VerifySession(ctx, st)
+	if err != nil {
+		fmt.Printf("WARNING - session manager failure: %s\n", utils.ErrorString(err))
+		// we do not return an error here, we give the authserver a chance to handle it
+		// long-term, it should not. this code needs to prove its reliability first though
+	}
+	if sv != nil && sv.IsValid {
+		// a valid session token detected
+		user_result = sv.User
+		goto got_user
+	}
+	// check the auth-server
 
 	// do the "real" lookup
-	gdr, err := AuthServer.SignedGetByToken(createBootstrapContext(), &apb.AuthenticateTokenRequest{Token: token})
+	ctx = createBootstrapContext()
+	gdr, err = AuthServer.SignedGetByToken(ctx, &apb.AuthenticateTokenRequest{Token: token})
 	if err != nil {
 		if *debug {
 			fmt.Printf("Error getting user for token %s: %s\n", token, err)
@@ -54,15 +79,16 @@ func TokenToUser(token string) (*apb.SignedUser, error) {
 		fmt.Println("Not valid")
 		return nil, fmt.Errorf("invalid user")
 	}
-	user := gdr.User
-	uu := common.VerifySignedUser(user)
+	user_result = gdr.User
+got_user:
+	uu := common.VerifySignedUser(user_result)
 	if uu == nil {
 		panic("signature invalid")
 	}
 	if !uu.Active {
-		cache_PutUser(token, user, fmt.Errorf("Useraccount deactivated"))
+		cache_PutUser(token, user_result, fmt.Errorf("Useraccount deactivated"))
 		return nil, fmt.Errorf("Useraccount deactivated")
 	}
-	cache_PutUser(token, user, nil)
-	return user, nil
+	cache_PutUser(token, user_result, nil)
+	return user_result, nil
 }
