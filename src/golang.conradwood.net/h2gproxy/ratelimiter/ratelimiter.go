b4374ee0fd7e7e8639d3c6ade5f929127a079fab
Conrad Wood <cnw@conradwood.net>
2023-12-12 09:34:50 +0000

    stall clients first, then 429
    Change-Id: I2ba68aa6302d46cc9598421c28818443a3495f20

--- src/golang.conradwood.net/h2gproxy/ratelimiter/ratelimiter.go
+++ src/golang.conradwood.net/h2gproxy/ratelimiter/ratelimiter.go
@@ -1,9 +1,14 @@
 package ratelimiter
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 	"time"
+)
+
+var (
+	stall_clients = flag.Bool("ratelimiter_stall", false, "if true, stall clients before returning 429")
 )
 
 type Limiter struct {
@@ -14,34 +19,83 @@ type Limiter struct {
 	AllowedClients int
 	// the back-queue... if we have more clients than that waiting we'll return immediately instead of stalling
 	AllowedWaitingClients int
-	//How many clients are currently processing
-	currentClients int
+	currentClients        int //How many clients are currently processing
+	currentWaiting        int // how many clients are currently waiting to be processed?
+	wait_chan             chan bool
 }
 
 func NewLimiter() *Limiter {
 	res := &Limiter{
+		wait_chan:             make(chan bool, 30),
 		AllowedClients:        50,
-		AllowedWaitingClients: 10,
-		MaxStall:              time.Duration(1) * time.Second,
+		AllowedWaitingClients: 100,
+		MaxStall:              time.Duration(3) * time.Second,
 	}
 	return res
 }
 
 func (l *Limiter) RequestStart() bool {
 	l.Lock()
-	defer l.Unlock()
 	if l.currentClients < l.AllowedClients {
 		l.currentClients++
+		l.Unlock()
 		return true
 	}
+	l.Unlock()
+	if !*stall_clients {
+		return false
+	}
+
+	l.Lock()
+	if l.currentWaiting >= l.AllowedWaitingClients {
+		// too many waiting already
+		l.Unlock()
+		return false
+	}
+	l.currentWaiting++
+	l.Unlock()
+
+	started := time.Now()
+	for {
+		select {
+		case <-l.wait_chan:
+			// continue because something has finished
+		case <-time.After(l.MaxStall / 3):
+			// timeout
+		}
+		// try to lock again,
+		l.Lock()
+		if l.currentClients < l.AllowedClients {
+			l.currentClients++
+			l.currentWaiting--
+			l.Unlock()
+			return true
+		}
+		l.Unlock()
+		if time.Since(started) >= l.MaxStall {
+			break
+		}
+	}
+	l.Lock()
+	l.currentWaiting--
+	l.Unlock()
 	return false
 }
 func (l *Limiter) RequestFinish() {
 	l.Lock()
-	defer l.Unlock()
 	l.currentClients--
 	if l.currentClients < 0 {
 		l.currentClients = 0
+	}
+	l.Unlock()
+	if !*stall_clients {
+		return
+	}
+	select {
+	case l.wait_chan <- true:
+		// sent
+	default:
+		// not sent
 	}
 }
 
