41d91b663ec8c55fc5be4d311e9f202aa4e4b655
Conrad Wood <cnw@conradwood.net>
2026-06-21 11:24:24 +0100

    automatic commit
    Change-Id: Ia78065414f870c557e551ddf0e55985494a9a6d5

--- src/golang.conradwood.net/h2gproxy/exe/shared/certs_test.go
+++ src/golang.conradwood.net/h2gproxy/exe/shared/certs_test.go
@@ -8,20 +8,40 @@ import (
 	"testing"
 	"time"
 
+	"golang.conradwood.net/go-easyops/errors"
 	xhttp "golang.conradwood.net/go-easyops/http"
 	"golang.conradwood.net/go-easyops/utils"
 )
 
+const (
+	//	TEST_HOST = "l.conradwood.net"
+	TEST_HOST = "www.carbonsaver.co.uk"
+)
+
 func TestChain(t *testing.T) {
+	stop()
 	h := &https_req_handler{}
 	h.test_start_server(t)
-	h.check_tls_server("127.0.0.1", 9323, "l.conradwood.net")
-
-	xhttp.Get("https://localhost:9323")
+	h.check_tls_server("127.0.0.1", 9323, TEST_HOST)
+	stop()
+	t.Logf("waiting for finish signal")
 	<-h.finish
+
 }
 
+func stop() {
+	xhttp.Get("https://" + TEST_HOST + ":9323")
+}
 func (h *https_req_handler) check_tls_server(addr string, port int, hostname string) {
+	h.t.Logf("priming certificate")
+	_, err := GetCert(TEST_HOST, time.Duration(5)*time.Second)
+	if err != nil {
+		h.t.Logf("failed to get cert: %s\n", errors.ErrorString(err))
+		h.t.Fail()
+		return
+	}
+	h.t.Logf("got certificate")
+
 	conf := &tls.Config{
 		InsecureSkipVerify: true,
 	}
@@ -37,14 +57,26 @@ func (h *https_req_handler) check_tls_server(addr string, port int, hostname str
 	certs := conn.ConnectionState().PeerCertificates
 	for i, cert := range certs {
 		h.t.Logf("Certificate #%d", i)
+		h.t.Logf("   Subject    : %s", cert.Subject)
 		h.t.Logf("   Issuer Name: %s", cert.Issuer)
 		h.t.Logf("   Expiry: %s", cert.NotAfter.Format("2006-January-02"))
 		h.t.Logf("   Common Name: %s", cert.Issuer.CommonName)
-
 	}
 }
 func (h *https_req_handler) test_start_server(t *testing.T) {
-	h.finish = make(chan bool)
+	// open the listener
+	l, err := net.Listen("tcp", ":9323")
+	if err != nil {
+		stop()
+		time.Sleep(time.Duration(1) * time.Second)
+		l, err = net.Listen("tcp", ":9323")
+		if err != nil {
+			t.Logf("Failed to listen: %s\n", err)
+			t.FailNow()
+			return
+		}
+	}
+	h.finish = make(chan bool, 10)
 	h.t = t
 	server := &http.Server{
 		//		Addr:    "127.0.0.1:9323",
@@ -60,13 +92,6 @@ func (h *https_req_handler) test_start_server(t *testing.T) {
 	//	tlsConfig.NameToCertificate = shared.CertMap()
 	tlsConfig.GetCertificate = test_getcert
 
-	// open the listener
-	l, err := net.Listen("tcp", ":9323")
-	if err != nil {
-		t.Logf("Failed to listen: %s\n", err)
-		t.FailNow()
-		return
-	}
 	// any incoming connections from now on will be queued by the OS
 	// and accepted after we call ServeTLS
 
@@ -79,8 +104,13 @@ func (h *https_req_handler) test_start_server(t *testing.T) {
 }
 
 func test_getcert(chi *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	fmt.Printf("Getcert for \"%s\"\n", chi.ServerName)
+	fmt.Printf(" [certhandler] Getcert for \"%s\"\n", chi.ServerName)
 	x, err := GetCert(chi.ServerName, time.Duration(5)*time.Second)
+	if err != nil {
+		fmt.Printf("  [certhandler] failed to get cert for \"%s\": %s\n", chi.ServerName, errors.ErrorString(err))
+	} else {
+		fmt.Printf(" [certhanlder] Gotcert for \"%s\"\n", chi.ServerName)
+	}
 	return x, err
 }
 
